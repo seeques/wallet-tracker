2b85f005c8c9c8160e2cda1687e4b78c39a23648
seeques <equestrianbuddy@gmail.com>
2025-12-07 11:52:13 +0300

    Print tracked address

--- internal/subscriber/subscriber.go
+++ internal/subscriber/subscriber.go
@@ -7,14 +7,26 @@ import (
 
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/common"
 )
+
+const listenAddr = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
 func SubscribeToBlocks(webSocketURL string) {
 	client, err := ethclient.Dial(webSocketURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
+	// TODO: check logic. defer actually never executes because of the infinite loop below
 	defer client.Close()
+
+	// Get chain ID to fetch tx's from address
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get network ID: %v", err)
+	}
+
+	mockAddress := common.HexToAddress(listenAddr)
 
 	headers := make(chan *types.Header)
 	// SubscribeNewHead returns subscription to recent headers that are added to our headers channel
@@ -30,6 +42,27 @@ func SubscribeToBlocks(webSocketURL string) {
 			log.Fatalf("Subscription error: %v", err)
 		case header := <-headers:
 			fmt.Printf("New block header: %v\n", header.Hash().Hex())
+
+			block, err := client.BlockByHash(context.Background(), header.Hash())
+			if err != nil {
+				log.Fatalf("Failed to get block: %v", err)
+			}
+
+			for _, tx := range block.Transactions() {
+				from, _ := types.Sender(types.NewLondonSigner(chainID), tx)
+				// TODO: need to think about quitting because of this
+				// if err != nil {
+				// 	log.Fatalf("Failed to get the sender: %v", err)
+				// }
+
+				if from == mockAddress {
+					fmt.Printf("Transaction from %s found in block %d: %s\n", listenAddr, block.Number().Uint64(), tx.Hash().Hex())
+				}
+				// check nil for contract creation
+				if tx.To() != nil && tx.To().Hex() == listenAddr {
+					fmt.Printf("Transaction to %s found in block %d: %s\n", listenAddr, block.Number().Uint64(), tx.Hash().Hex())
+				}
+			}
 		}
 	}
 
