b8a7b19b7cad783ccf879411d198477c459b382b
seeques <equestrianbuddy@gmail.com>
2025-12-07 13:21:23 +0300

    Fix errors

--- internal/fetcher/tracker.go
+++ internal/fetcher/tracker.go
@@ -3,37 +3,39 @@ package fetcher
 import (
 	"context"
 	"fmt"
-	"log"
 
-	// "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func TrackWallets(client *ethclient.Client, header *types.Header, addresses map[common.Address]bool) {
+func TrackWallets(client *ethclient.Client, header *types.Header, addresses map[common.Address]bool) error {
 	// Get chain ID to fetch tx's from address
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to get network ID: %v", err)
+		return fmt.Errorf("Failed to get network ID: %v", err)
 	}
 
 	fmt.Printf("New block header: %v\n", header.Hash().Hex())
 
-	block, _ := client.BlockByHash(context.Background(), header.Hash())
+	block, err := client.BlockByHash(context.Background(), header.Hash())
+	if err != nil {
+		return fmt.Errorf("Failed to fetch block by hash: %v", err)
+	}
 
 	for _, tx := range block.Transactions() {
 		from, err := types.Sender(types.NewLondonSigner(chainID), tx)
 		if err != nil {
-			log.Fatalf("Failed to get the sender: %v", err)
+			return fmt.Errorf("Failed to get the sender: %v", err)
 		}
 
-		if addresses[from] == true {
+		if addresses[from] {
 			fmt.Printf("Spotted from address %s in tx %s\n", from.Hex(), tx.Hash().Hex())
 		}
 
-		if addresses[*tx.To()] == true {
+		if addresses[*tx.To()] && tx.To != nil {
 			fmt.Printf("Spotted to address %s in tx %s\n", tx.To().Hex(), tx.Hash().Hex())
 		}
 	}
+	return nil
 }
