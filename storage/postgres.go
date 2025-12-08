ca2a8fb6e7eb7781e73fa305f54a776b4e1f80c8
seeques <equestrianbuddy@gmail.com>
2025-12-08 19:48:18 +0300

    Implement Storage interface, fix CreatePool func

--- storage/postgres.go
+++ storage/postgres.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -27,13 +26,20 @@ type Storage interface {
 	SaveTransaction(ctx context.Context, tx *TrackedTransaction) error
 }
 
+func (s *PostgresStorage) SaveTransaction(ctx context.Context, tx *TrackedTransaction) error {
+	sql := `INSERT INTO transactions (hash, block_number, from_address, to_address, val, tmstp) VALUES ($1, $2, $3, $4, $5, $6)`
+
+	_, err := s.pool.Exec(ctx, sql, tx.Hash, tx.BlockNumber, tx.From, tx.To, tx.Value, tx.Timestamp)
+	return err
+}
+
 // Create a constructor function
 func NewPgStorage(pool *pgxpool.Pool) *PostgresStorage {
 	return &PostgresStorage{pool: pool}
 }
 
-func CreatePool() (*Pool, error) {
-	conn, err := pgxpool.Connect(context.Background(), "postgres://tracker:tracker@localhost:5432/wallet_tracker?sslmode=disable")
+func CreatePool() (*pgxpool.Pool, error) {
+	conn, err := pgxpool.New(context.Background(), "postgres://tracker:tracker@localhost:5432/wallet_tracker?sslmode=disable")
 	if err != nil {
 		return nil, err
 	}
