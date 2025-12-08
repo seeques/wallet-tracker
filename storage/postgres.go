afd2894dbbc1840a932fdae609cd604caa437037
seeques <equestrianbuddy@gmail.com>
2025-12-08 19:22:51 +0300

    Add storage interface and struct with sql fields

--- storage/postgres.go
+++ storage/postgres.go
@@ -1,13 +1,27 @@
-package storage 
+package storage
 
 import (
 	"context"
 	"fmt"
+	"math/big"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // connection string: postgres://tracker:tracker@localhost:5432/wallet_tracker?sslmode=disable
+
+type TrackedTransaction struct {
+	Hash        string
+	BlockNumber uint64
+	From        string
+	To          string
+	Value       *big.Int
+	Timestamp   uint64
+}
+
+type Storage interface {
+	SaveTransaction(ctx context.Context, tx *TrackedTransaction) error
+}
 
 func CreatePool() (*Pool, error) {
 	conn, err := pgxpool.Connect(context.Background(), "postgres://tracker:tracker@localhost:5432/wallet_tracker?sslmode=disable")
